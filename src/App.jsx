23ea47d5d2f0356ea22ecb8e0579ca92fdd1cf50
Zainab <156816915+zainabreh@users.noreply.github.com>
2024-08-03 13:17:45 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import React from 'react'
-import { createRoutesFromElements, Route, RouterProvider } from 'react-router'
-import Layout from './components/Layout'
-import Home from './pages/Home'
-import { createBrowserRouter } from 'react-router-dom'
 import './App.css'
 
 const App = () => {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path='/' element={<Layout/>}>
-        <Route index element={<Home/>}/>
-      </Route>
-    )
-  )
+ 
   return (
     <>
     
-    <RouterProvider router={router}/>
+   
      
     </>
   )
